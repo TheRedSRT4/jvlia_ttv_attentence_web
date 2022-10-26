a15957ea0ea2649033252f5d2036f8b36ccb9bf9
TheRedSRT4 <FadeShotEdits@gmail.com>
2022-10-25 21:00:49 -0400

    First commit

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/ash
 rm -rf /home/container/tmp/*
 
-echo "⟳ Starting PHP-FPM..."
-/usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
-
 echo "⟳ Starting Nginx..."
 echo "✓ Successfully started"
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
+
+echo "⟳ Starting Core.jar..."
+echo "✓ Successfully started"
+java -jar core.jar
