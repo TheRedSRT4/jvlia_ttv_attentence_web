5a9210e132c1e48a9c637de68ef39e69e0628a93
TheRedSRT4 <FadeShotEdits@gmail.com>
2022-10-26 20:05:52 -0400

    modified Dockerfile and start.sh

--- Dockerfile
+++ Dockerfile
@@ -9,8 +9,6 @@ ENV  USER container
 ENV HOME /home/container
 
 WORKDIR /home/container
-RUN curl https://github.com/TheRedSRT4/JVLIA_TTV_ATTENDANCE/releases/download/latest/core.jar
-RUN curl https://github.com/TheRedSRT4/JVLIA_TTV_ATTENDANCE/releases/download/latest/stats.jar
 COPY ./entrypoint.sh /entrypoint.sh
 
 
--- start.sh
+++ start.sh
@@ -5,6 +5,10 @@ echo "⟳ Starting Nginx..."
 echo "✓ Successfully started"
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
 
+
+echo "⟳ Downloading core.jar and stats.jar..."
+curl https://github.com/TheRedSRT4/JVLIA_TTV_ATTENDANCE/releases/download/latest/core.jar
+curl https://github.com/TheRedSRT4/JVLIA_TTV_ATTENDANCE/releases/download/latest/stats.jar
 echo "⟳ Starting Core.jar..."
 echo "✓ Successfully started"
 java -jar core.jar
